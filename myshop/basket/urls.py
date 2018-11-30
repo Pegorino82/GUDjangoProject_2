914016cb380c5e205532470bbff1c49ebac0c5c6
Pegorino82 <eshkryabin@yandex.ru>
2018-11-30 13:20:11 +0300

    CBV for list

--- myshop/basket/urls.py
+++ myshop/basket/urls.py
@@ -5,6 +5,8 @@ from basket import views
 app_name = 'basketapp'
 
 urlpatterns = [
+    path('list/', views.ModelBasketList.as_view(), name='list'),
+
     path('basket/', views.basket, name='basket'),
     path('add_product/<int:pk>/', views.add_product, name='add_product'),
     path('remove_product/<int:pk>/', views.remove_product, name='remove_product'),
