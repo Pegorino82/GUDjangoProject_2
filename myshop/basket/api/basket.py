5655bb6a15da7a00b9e7a01bd0ebdfa2d7db06d6
Pegorino82 <eshkryabin@yandex.ru>
2018-11-30 13:44:28 +0300

    fix bug when user is anonimous

--- myshop/basket/api/basket.py
+++ myshop/basket/api/basket.py
@@ -1,25 +1,29 @@
 import json
 from django.shortcuts import render, redirect, get_object_or_404, get_list_or_404
 
-from django.http import JsonResponse
+from django.http import JsonResponse, Http404, HttpResponseNotFound
 from django.core import serializers
 
 from basket.models import Basket
-from customers.models import Customer
 
 
 def rest_basket_list(request):
     user = request.user
-    obj_list = get_list_or_404(Basket, user=user)
-    data = [obj.__dict__ for obj in obj_list]
-    for i in data:
-        i.pop('_state')
+    if not user.is_anonymous:
+        obj_list = get_list_or_404(Basket, user=user)
+        data = [obj.__dict__ for obj in obj_list]
+        for i in data:
+            i.pop('_state')
+        data = list()
 
-    return JsonResponse(
-        {
-            'results': data,
-        }
-    )
+        return JsonResponse(
+            {
+                'results': data,
+            }
+        )
+    else:
+        raise Http404('No Basket matches the given query.')
+        # return HttpResponseNotFound('<p>No Basket matches the given query.</p>')
 
 
 def rest_basket_detail(request):
@@ -33,7 +37,6 @@ def rest_basket_detail(request):
             'results': data,
         }
     )
-
 
 
 def rest_basket_update(request):
