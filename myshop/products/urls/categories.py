81b0ef37e9ced809806ba87cad730167eba81dc4
pegorino <eshkryabin@yandex.ru>
2019-01-22 13:13:34 +0300

    закешировал страницы товаров одной категории

--- myshop/products/urls/categories.py
+++ myshop/products/urls/categories.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from products.views import (
     category,
@@ -13,7 +14,7 @@ app_name = 'categoriesapp'
 
 urlpatterns = [
     # path('category/<str:category>/', category, name='category'),
-    path('category/<int:pk>/', CategoryListOfView.as_view(), name='category'),  # все продукты категории
+    path('category/<int:pk>/', cache_page(3600)(CategoryListOfView.as_view()), name='category'),  # все продукты категории
     path('create/', CategoryCreateView.as_view(), name='create'),
     path('list/', CategoryListView.as_view(), name='list'),
     path('update/<int:pk>/', CategoryUpdateView.as_view(), name='update'),
